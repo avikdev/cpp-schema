ffcbd6bc55dd99f22ef50b39bc78d78725ffee11
Avik Paul <65626322+avikdev@users.noreply.github.com>
2026-02-11 06:27:46 -0800

    Clarify comment for VoidType struct
    Updated comment to clarify the purpose of the VoidType struct.
    And added newline at the end of file.

--- cppschema/common/types.h
+++ cppschema/common/types.h
@@ -1,6 +1,6 @@
 #pragma once
 
-// The sentinel struct representing a C++ `void` type in JavaScript.
+// This sentinel struct represents a C++ `void` type with an empty object JavaScript.
 // Since `void` cannot be used in references, we use this type in APIs which has no arg, or
 // does not return a value.
 struct VoidType {};
